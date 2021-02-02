0782634836ad0a85bb613a108b63747cbe4b1c7e
beyerja <beyerja@gmx.de>
2021-02-02 18:54:24 +0100

    Add file search helper function
    - Add file search helper function.

--- py/IO/SysHelpers.py
+++ py/IO/SysHelpers.py
@@ -6,12 +6,22 @@
 # ------------------------------------------------------------------------------
 
 from pathlib import Path
+import os
 
 # ------------------------------------------------------------------------------
+# Creating
 
 def create_dir(dir):
   """ Try to create the given directory and it's parents.
   """
   Path(dir).mkdir(parents=True, exist_ok=True)
+
+# ------------------------------------------------------------------------------
+# Finding
+
+def find_files(dir,extension):
+  """ Find all the files of a given extension in the directory.
+  """
+  return["{}/{}".format(dir,dir_object) for dir_object in os.listdir(dir) if extension in dir_object]
 
 # ------------------------------------------------------------------------------
