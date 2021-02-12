c4d1dad0804ab83e7b2c499ce9cd498f96edc200
beyerja <beyerja@gmx.de>
2021-02-12 18:56:56 +0100

    Add color spectrum helper
    - Add color spectrum helper class.

--- py/PlottingHelp/Colors.py
+++ py/PlottingHelp/Colors.py
@@ -7,7 +7,6 @@
 
 from matplotlib import cm
 
-#-------------------------------------------------------------------------------
 
 class ColorMap:
   """ A colormap class using a matplotlib color map to draw colors for a given 
@@ -20,6 +19,24 @@ class ColorMap:
   def __getitem__(self,index):
     """ Define what happens when the [] operator is applied (only reading).
     """
-    return self.colormap(float(index)/float(self.n_colors))
+    return self.colormap(float(index)/float(self.n_colors-1))
+    
+#-------------------------------------------------------------------------------
+
+class ColorSpectrum:
+  """ A colormap class using a matplotlib color map to draw colors for a range 
+      of values between given minimum and maximum.
+  """
+  def __init__(self,cm_name,v_min,v_max):
+    self.colormap = cm.get_cmap(cm_name)
+    self.v_min = v_min
+    self.v_max = v_max
+    
+  def __getitem__(self,index):
+    """ Define what happens when the [] operator is applied (only reading).
+    """
+    if (index < self.v_min) or (index > self.v_max):
+      raise ValueError("Requested color for index {} out of range [{},{}].".format(index,self.v_min,self.v_max))
+    return self.colormap(float(index)/float(self.v_max - self.v_min))
     
 #-------------------------------------------------------------------------------
