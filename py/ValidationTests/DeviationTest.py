6110996b8a4f21c3453be582db68a5c08f6228d5
beyerja <beyerja@gmx.de>
2021-02-12 18:58:14 +0100

    New color scheme and more plots in deviation test
    - Change coloring scheme in deviation plots.
    - Add additional deviation plots for absolute change from 
    parametrisation.

--- py/ValidationTests/DeviationTest.py
+++ py/ValidationTests/DeviationTest.py
@@ -16,6 +16,7 @@ import Colors as PHC
 import Markers as PHM
 sys.path.append("../ValidityMeasures")
 import CommonConfig as VMCC
+import DeltaHelp as VMDH
 
 #-------------------------------------------------------------------------------
 
@@ -64,7 +65,11 @@ def plot_deviation_test(file_path, output_formats=["pdf"]):
   # Find the bin edges for each dimension
   bin_edges = [np.linspace(reader["CoordMin"][d],reader["CoordMax"][d],reader["CoordNBins"][d]+1) for d in range(n_dims)]
   
+  # Find the deltas and the minimum and maximum deviations
   deltas = IOPH.delta_pairs(df)
+  delta_metrics = VMDH.delta_metric(deltas)
+  colors = PHC.ColorSpectrum("plasma",np.amin(delta_metrics),1.1*np.amax(delta_metrics))
+  
   for dev_dir in dev_directions:
     log.debug("Looking at direction {}".format(dev_dir.name))
     dir_selection = dev_dir.func(deltas)
@@ -75,6 +80,7 @@ def plot_deviation_test(file_path, output_formats=["pdf"]):
     N_par = np.array([dir_rows["P{}".format(b)] for b in range(n_bins)])
     
     diff_c0 = (N_cut - N_cut_cut0)*scale_factor
+    diff_p0 = (N_par - N_cut_cut0)*scale_factor
     diff_pc = (N_par - N_cut)*scale_factor
     
     for d in range(n_dims):
@@ -109,12 +115,9 @@ def plot_deviation_test(file_path, output_formats=["pdf"]):
 
       y_bin_edges = np.linspace(np.amin(diff_c0), np.amax(diff_c0), 20)
 
-      colors = PHC.ColorMap("plasma",len(dir_deltas))
-
-      # the scatter plot:
       for row in range(len(dir_deltas)):
         y = diff_c0[:,row]
-        color = colors[row]
+        color = colors[VMDH.delta_metric(dir_deltas[row])[0]]
         scatter = ax_scatter.scatter(x, y, color='none', edgecolors=color, marker=PHM.markers[row], label=r"$({} \delta, {} \delta)$".format(dir_deltas[row][0]/reader["Delta"],dir_deltas[row][1]/reader["Delta"]))
 
         ax_histx.hist(x, bins=bin_edges[d], weights=y, histtype='step',fill=False, ec=color)
@@ -131,6 +134,56 @@ def plot_deviation_test(file_path, output_formats=["pdf"]):
         IOSH.create_dir(format_dir)
         dev_dir_name = dev_dir.name.replace(" ", "_")
         fig.savefig("{}/{}_{}_DevCutCut0_{}.{}".format(format_dir, base_name, reader["CoordName"][d], dev_dir_name, format))
+      
+      plt.close(fig)
+      
+      #--- Plot N_par - N_cut0 -------------------------------------------------
+      
+      # start with a rectangular Figure
+      fig = plt.figure(figsize=(6, 6))
+      fig.suptitle(title)#, fontsize=16)
+      
+      # definitions for the axes
+      left, width = 0.15, 0.49
+      bottom, height = 0.1, 0.49
+      spacing = 0.005
+      rect_scatter = [left, bottom, width, height]
+      rect_histx = [left, bottom + height + spacing, width, 0.93 - (bottom + height + spacing)]
+      rect_histy = [left + width + spacing, bottom, 0.93 - (left + width + spacing), height]
+      leg_pos = [(width + spacing)/width, (height + spacing)/height]
+      
+      ax_scatter = plt.axes(rect_scatter)
+      ax_scatter.tick_params(direction='in', top=True, right=True)
+      ax_scatter.set_xlabel(reader["CoordName"][d])
+      ax_scatter.set_ylabel(r"$N_{par}^{(\Delta c,\Delta w)} - N_{cut}^{0}$")
+      ax_histx = plt.axes(rect_histx)
+      ax_histx.tick_params(direction='in', labelbottom=False)
+      ax_histx.set_ylabel("Cumulative")
+      ax_histy = plt.axes(rect_histy)
+      ax_histy.tick_params(direction='in', labelleft=False)
+      ax_histy.set_xlabel("#bins")
+
+      y_bin_edges = np.linspace(np.amin(diff_p0), np.amax(diff_p0), 20)
+
+      for row in range(len(dir_deltas)):
+        y = diff_p0[:,row]
+        color = colors[VMDH.delta_metric(dir_deltas[row])[0]]
+        scatter = ax_scatter.scatter(x, y, color='none', edgecolors=color, marker=PHM.markers[row], label=r"$({} \delta, {} \delta)$".format(dir_deltas[row][0]/reader["Delta"],dir_deltas[row][1]/reader["Delta"]))
+
+        ax_histx.hist(x, bins=bin_edges[d], weights=y, histtype='step',fill=False, ec=color)
+        ax_histy.hist(y, bins=y_bin_edges, orientation='horizontal', histtype='step',fill=False, ec=color)
+
+      ax_histx.set_xlim(ax_scatter.get_xlim())
+      ax_histy.set_ylim(ax_scatter.get_ylim())
+      
+      ax_scatter.legend(loc=leg_pos, title=r"$(\Delta c, \Delta w), \delta={}$".format(reader["Delta"]))
+      
+      # Save the figure in all requested formats
+      for format in output_formats:
+        format_dir = "{}/{}/DevParCut0".format(output_dir,format)
+        IOSH.create_dir(format_dir)
+        dev_dir_name = dev_dir.name.replace(" ", "_")
+        fig.savefig("{}/{}_{}_DevParCut0_{}.{}".format(format_dir, base_name, reader["CoordName"][d], dev_dir_name, format))
       
       plt.close(fig)
       
@@ -162,12 +215,9 @@ def plot_deviation_test(file_path, output_formats=["pdf"]):
 
       y_bin_edges = np.linspace(np.amin(diff_pc), np.amax(diff_pc), 20)
 
-      colors = PHC.ColorMap("plasma",len(dir_deltas))
-
-      # the scatter plot:
       for row in range(len(dir_deltas)):
         y = diff_pc[:,row]
-        color = colors[row]
+        color = colors[VMDH.delta_metric(dir_deltas[row])[0]]
         scatter = ax_scatter.scatter(x, y, color='none', edgecolors=color, marker=PHM.markers[row], label=r"$({} \delta, {} \delta)$".format(dir_deltas[row][0]/reader["Delta"],dir_deltas[row][1]/reader["Delta"]))
 
         ax_histx.hist(x, bins=bin_edges[d], weights=y, histtype='step',fill=False, ec=color)
